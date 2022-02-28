1069ae468f61eae93ec69845a9c24afda8400b08
Alejandro Rivera <jarivera636@misena.edu.co>
2022-02-28 02:55:07 -0500

    Atribute with form reactives in login

--- front-End/src/app/components/login/login.component.ts
+++ front-End/src/app/components/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Import module Form Reactive */
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+    /* attribute FormReactive in login  */
 
-  constructor() { }
+    loginForm: FormGroup;
+    validationMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+    validationPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            user: [
+                '',
+                [Validators.required, Validators.pattern(this.validationMail)],
+            ],
+            password: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern(this.validationPassword),
+                ],
+            ],
+        });
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    successAccessUser() {
+        console.log(this.loginForm);
+    }
 }
